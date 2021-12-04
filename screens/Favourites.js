6b8d3d65fecbd6ca8cf7272aed1cd8e6d2d157dd
SofranSebastian <sofransebastian@yahoo.com>
2021-12-04 16:31:00 +0200

    enhanced the basic layout

--- screens/Favourites.js
+++ screens/Favourites.js
@@ -5,15 +5,70 @@ import Constants from '../StoredData.js';
 import { Button, Modal, Portal, Provider, Avatar } from 'react-native-paper';
 import NavigationScreen from './NavigationScreen.js';
 import StickyParallaxHeader from 'react-native-sticky-parallax-header';
-import CardHospital from '../components/CardHospital.js';
+import CardFavourite from '../components/CardFavourite.js';
 import Loading from '../components/Loading.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Favourites({route, navigation}){
 
+    const{ userLat, userLon } = route.params;
 
-    return(
-            <View>
-                <Text>Favourites Screen</Text>
+    const[ listaFavorite, setListaFavorite ] = useState([]);
+    const[ spitaleIncarcate, setSpitaleIncarcate ] = useState(false);
+
+    async function getFavouritesList(){
+        try{
+            var list = await AsyncStorage.getItem('favouritesList');
+            if( list ){
+                setListaFavorite( JSON.parse(list) );
+                setSpitaleIncarcate( true );
+            }else{
+                list = [];
+                setSpitaleIncarcate( true );
+            }
+        }catch(e){
+            //e
+        }
+    }
+
+    useEffect(()=> getFavouritesList(),[spitaleIncarcate])
+
+    if( spitaleIncarcate ){
+        return(
+            <View style={{flex:1, backgroundColor:"white"}}>
+
+                <View style={{marginTop:'25%'}}>
+                    <FlatList 
+                            data={listaFavorite}
+                            renderItem={ ({item}) => 
+                                <CardFavourite  title={item.name}
+                                                adress={item.adress}
+                                                userLat={userLat}
+                                                userLon={userLon}
+                                                destLat={item.destLat}
+                                                destLon={item.destLon}
+                                                destName={item.name}
+                                                navigation={navigation}
+                                />
+                        
+                            }
+                            keyExtractor={ (item) => item.name }
+                    />
+                </View>
+                <Button onPress={ () => navigation.navigate("HeatMap")}>
+                    back
+                </Button>
             </View>
-    )
+        )
+    }else{
+        return(
+            <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'white'}}>
+                <Loading isDataLoading={true}/>
+                <Text style={{fontSize:20, fontFamily:'bold-font', textAlign:'center', color:'#094AA8', marginTop:'5%', padding:10}}>
+                    Loading
+                </Text>
+            </View>
+        )
+    }
+    
 }
