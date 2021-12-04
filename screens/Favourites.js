9c0c475ca96eaf08c3509ffeba47237eb914a4c5
SofranSebastian <sofransebastian@yahoo.com>
2021-12-04 19:12:09 +0200

    Favourites screen added new layout

--- screens/Favourites.js
+++ screens/Favourites.js
@@ -35,30 +35,52 @@ export default function Favourites({route, navigation}){
 
     if( spitaleIncarcate ){
         return(
-            <View style={{flex:1, backgroundColor:"white"}}>
-
-                <View style={{marginTop:'25%'}}>
-                    <FlatList 
-                            data={listaFavorite}
-                            renderItem={ ({item}) => 
-                                <CardFavourite  title={item.name}
-                                                adress={item.adress}
-                                                userLat={userLat}
-                                                userLon={userLon}
-                                                destLat={item.destLat}
-                                                destLon={item.destLon}
-                                                destName={item.name}
-                                                navigation={navigation}
-                                />
+                        <StickyParallaxHeader   headerType="AvatarHeader" 
+                        backgroundColor='#094AA8'
+                        rightTopIcon={{}}
+                        leftTopIcon={require("../assets/back.png")}
+                        leftTopIconOnPress={ () => navigation.reset({
+                            index: 0,
+                            routes: [{ name: 'HeatMap' }]
+                       }) }
+                        image={{}}
+                        parallaxHeight={225}
+                        foreground={    ()  =>
+                                            <View style={{flex:1, padding:10, borderRadius:0}}>
+                                                <View style={{alignItems:'flex-start'}}>
+                                                    <Image source={require("../assets/favourites_artboard.png")} style={{width:400, height:110}} resizeMode="contain" />
+                                                </View>
+                                                <Text style={{fontSize:24, fontFamily:'bold-font', textAlign:'left', color:"white", marginTop:'5%'}}>
+                                                    FAVOURITE MEDICAL CENTERS
+                                                </Text>
+                                            </View>
+                        }
+                        title="FAVOURITE CENTERS"
                         
-                            }
-                            keyExtractor={ (item) => item.name }
-                    />
-                </View>
-                <Button onPress={ () => navigation.navigate("HeatMap")}>
-                    back
-                </Button>
-            </View>
+                        children={  
+                            <View style={{flex:1, backgroundColor:'white'}}>
+                            <FlatList 
+                                    data={listaFavorite}
+                                    renderItem={ ({item}) => 
+                                        <CardFavourite  title={item.name}
+                                                        adress={item.adress}
+                                                        userLat={userLat}
+                                                        userLon={userLon}
+                                                        destLat={item.destLat}
+                                                        destLon={item.destLon}
+                                                        destName={item.name}
+                                                        navigation={navigation}
+                                        />
+                                
+                                    }
+                                    keyExtractor={ (item) => item.name }
+                            />
+                        </View>
+                                    
+                                        // listaSpitale.map( (item) => ( <View key={item.key}><Text>{item.placeName}</Text></View> ) )
+                                    
+                                }
+/>
         )
     }else{
         return(
